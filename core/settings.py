0e09b634d2aecb8a2e8e13aea88316bb3fc6c23d
Sonu Kumar Mishra <mishrasonu9211@gmail.com>
2023-02-21 18:57:15 +0530

    secret key removed

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+3x$v!t_o(^6(x1_8i@61^=-^9&njkqd%a4yz$zbqm6ea1e-tr'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
